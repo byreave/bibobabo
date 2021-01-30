954b64d34ea4b3d0304bc6a6c28915a8fc6ebf0b
Byreave Luo <654297651@qq.com>
2021-01-30 17:17:38 +0800

    add player movements controller

---
+++ Assets/Script/Character/MovementControl/CharacterMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [Tooltip("Force to rotate the player.")]
+    public float PlayerAngularForce = 100.0f;
+
+    private Rigidbody PlayerRB;
+    private float PlayerMass = 100.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the player
+        float MovementInput = Input.GetAxis("Horizontal");
+        PlayerMove(-MovementInput);
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    void PlayerMove(float inputVal)
+    {
+        if(PlayerRB)
+        {
+            PlayerRB.AddTorque(Vector3.forward * PlayerAngularForce / PlayerMass * inputVal, ForceMode.Acceleration);
+            // Drag is handled by the physics system itself...
+        }
+    }
+
+    // Update the player mass, which decides the player's physics interactions.
+    public void UpdatePlayerMass(float inPlayerMass)
+    {
+        PlayerMass = inPlayerMass;
+    }
+
+    public float GetPlayerMass()
+    {
+        return PlayerMass;
+    }
+}
---
+++ Assets/Script/Gameplay/WindfieldController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindfieldController : MonoBehaviour
+{
+    [Tooltip("How much force the wind field does to the player.")]
+    public float WindfieldForce = 100.0f;
+    [Tooltip("To which direction the wind blows. Default Y Axis")]
+    public Vector3 WindDirection = new Vector3(0.0f, 1.0f, 0.0f);
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        GameObject PlayerObject = other.gameObject;
+        if (PlayerObject.CompareTag("Player"))
+        {
+            Debug.Log("Player Enter");
+            WindDirection.Normalize();
+            CharacterMovement PlayerMovement = PlayerObject.GetComponent<CharacterMovement>();
+            Rigidbody PlayerRb = PlayerObject.GetComponent<Rigidbody>();
+            if (PlayerMovement && PlayerRb)
+            {
+                float PlayerMass = PlayerMovement.GetPlayerMass();
+                PlayerRb.AddForce(WindDirection * WindfieldForce / PlayerMass, ForceMode.Acceleration);
+            }
+        }
+    }
+}
