a8feba5980c2a7022a89a3bc4b5fc888f6b89395
RongdaWei <victor.wei1994@gmail.com>
2021-01-31 12:45:58 +0800

    More stuff
    123

--- Assets/Scenes/Camera_Zoom.cs
+++ Assets/Scenes/Camera_Zoom.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class Camera_Zoom : MonoBehaviour
 {
+    void public GameObject Camera;
+    void public float ZoomOutDistance;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            
+        }
+    }
     // Update is called once per frame
     void Update()
     {
